b5af39cf2376587922230402781669aaa1011c00
Max <penarlmax@gmail.com>
2023-04-25 22:06:23 -0500

    Go go

---
+++ Source/RenderClass/RenderClassGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RenderClassGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RENDERCLASS_API ARenderClassGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
